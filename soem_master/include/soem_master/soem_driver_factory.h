35f303236d2857d5b75a9a6e2d0fadfb3cacf20a
Keivan Zavari <keivan.zavari@frsrobotics.com>
2017-10-20 17:11:58 +0200

    a bit of doxygen style documentation is added

--- soem_master/include/soem_master/soem_driver_factory.h
+++ soem_master/include/soem_master/soem_driver_factory.h
@@ -38,33 +38,81 @@ namespace soem_master
 class SoemDriverFactory
 {
 public:
+    //! gives you a static instance of the class
+    /**
+    *  @return a static reference to SoemDriverFactory class.
+    * On the object you can call its createDriver method.
+    */
     static SoemDriverFactory& Instance()
     {
         static SoemDriverFactory soem_driver_factory;
         return soem_driver_factory;
     }
 
+    // typedef int (*PFI)(char *, char *);
+    // creates PFI as a synonym for a pointer to a function of
+    // two char * arguments that returns an int.
+    /** Made for ease of use in SoemDriver::registerDriver
+    *
+    * creates CreateDriverCallBack as a synonym for a pointer to a function of
+    * ec_slavet* argument that returs a SoemDriver*
+    */
     typedef SoemDriver* (*CreateDriverCallBack)(ec_slavet*);
 
+    /**
+    * registers the driver, it is only used in a few places:
+    * - soem_ebox/src/soem_ebox.cpp: registerDriver("E/BOX",...
+    * - soem_beckhoff_drivers/src/soem_el3062.cpp: registerDriver("EL3062",...
+    * why? not documented...
+    *
+    * it basically inserts the given parameters to the FactoryMap type
+    * @param std::string name
+    * @param CreateDriverCallBack type defined here
+    *
+    * @return
+    * - true if a new element was inserted or
+    * - false if an equivalent key already existed.
+    */
     bool registerDriver(std::string name, CreateDriverCallBack createFn);
 
+    /**
+    * Creates the driver by looking up its name in the map
+    * @return a pointer to SoemDriver class.
+    * @param pointer of ec_slavet
+    *
+    * ec_slavet is a structure defined in ../../soem_core/ethercatmain
+    */
     SoemDriver* createDriver(ec_slavet* mem_loc);
     void displayAvailableDrivers();
 
 private:
+    /** Made for ease of use
+    * creates the map of names and CreateDriverCallBack which is descibed above
+    */
     typedef std::map<std::string, CreateDriverCallBack> FactoryMap;
+
+    /// std::map that contains the names and reference to the slaves
     FactoryMap m_factory_map;
 
-    SoemDriverFactory()
-    {
-    }
-    ;
+    //! Empty constructor
+    /**
+    * - used in SoemDriverFactory::Instance method
+    */
+    SoemDriverFactory(){};
+
+    /** Constructor
+    * - with a constant reference to SoemDriverFactory objects
+    * since we want to make a static object...
+    */
     SoemDriverFactory(const SoemDriverFactory&);
+
+    /** Destructor is empty
+    */
+    ~SoemDriverFactory(){};
+
+    /// equal operator
     SoemDriverFactory& operator=(const SoemDriverFactory&);
-    ~SoemDriverFactory()
-    {
-    }
-    ;
+
 };
 }
 #endif
